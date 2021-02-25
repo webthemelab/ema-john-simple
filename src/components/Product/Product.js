7c2b892aa5a2e256ed147b0f7e9dee105286f1e0
webthemelab <75390272+webthemelab@users.noreply.github.com>
2021-02-26 04:40:27 +0600

    ok

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './product.css'
 
 const Product = (props) => {
@@ -15,6 +17,7 @@ const Product = (props) => {
                 <p><small>by:{seller}</small></p>
                 <p>${price}</p>
                 <p><small>only {stock} left in stock - order soon</small></p>
+                <button className="main-button"> <FontAwesomeIcon icon={faShoppingCart} /> Add to cart</button>
             </div>
         </div>
     );
