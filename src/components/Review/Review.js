e97c6238f78a09417a8525e0441a792959c3effe
webthemelab <75390272+webthemelab@users.noreply.github.com>
2021-03-04 16:06:50 +0600

    almost ok

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import fakeData from '../../fakeData';
+import { getDatabaseCart } from '../../utilities/databaseManager';
+import ReviewItem from '../ReviewItem/ReviewItem';
 
 const Review = () => {
+    const [ cart, setCart] = useState([]);
+    useEffect(() =>{
+        //cart
+        const saveCart = getDatabaseCart();
+        const productKeys = Object.keys(saveCart);
+        const cartProducts = productKeys.map( key => {
+            const product = fakeData.find( pd => pd.key === key);
+            product.quantity = saveCart[key];
+            return product;
+        },[]);
+        setCart(cartProducts);
+    })
     return (
         <div>
-            <h1>Review</h1>
+            <h1> Cart items: {cart.length}</h1>
+            {
+                cart.map(pd => <ReviewItem 
+                    key ={pd.key}
+                    product={pd}></ReviewItem>)
+            }
         </div>
     );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import fakeData from '../../fakeData';
+import { addToDatabaseCart } from '../../utilities/databaseManager';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -11,6 +12,10 @@ const Shop = (product) => {
     const handleAddProduct =(product) =>{
         const newCart = [...cart, product];
         setCart(newCart);
+
+        const sameProduct = newCart.filter(pd => pd.key === product.key);
+        const count = sameProduct.length;
+        addToDatabaseCart(product.key, count);
     }
     
     
@@ -20,6 +25,7 @@ const Shop = (product) => {
             <div className="product-container">
                 {
                     products.map(pd => <Product 
+                        key = {pd.key}
                         showAddToCart ={true}
                         handleAddProduct ={handleAddProduct}
                         product ={pd}>
